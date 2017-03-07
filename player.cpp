3062b40577dc6bdfcfcbb80d397222d9143b9c78
David Adams <dmadams@caltech.edu>
2017-03-07 00:28:27 -0800

    Beats SimplePlayer

--- player.cpp
+++ player.cpp
@@ -3,27 +3,34 @@
 // Another try
 
 #include "player.hpp"
-
+int valueMap[64] = {8, -6, 3, 3, 3, 3, -6, 8,
+                    -6, -8, -1, -1, -1, -1, -8, -6,
+                    3, -1, 2, 1, 1, 2, -1, 3,
+                    3, -1, 1, 1, 1, 1, -1, 3,
+                    3, -1, 1, 1, 1, 1, -1, 3,
+                    3, -1, 2, 1, 1, 2, -1, 3,
+                    -6, -8, -1, -1, -1, -1, -8, -6,
+                    8, -6, 3, 3, 3, 3, -6, 8};
 /*
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
  * within 30 seconds.
  */
-Player::Player(Side side) {
-    // Will be set to true in test_minimax.cpp.
+Player::Player(Side side) 
+{
     testingMinimax = false;
+    this->side = side;
+    board = new Board();
+    score = 0;
 
-    /*
-     * TODO: Do any initialization you need to do here (setting up the board,
-     * precalculating things, etc.) However, remember that you will only have
-     * 30 seconds.
-     */
 }
 
 /*
  * Destructor for the player.
  */
-Player::~Player() {
+Player::~Player() 
+{
+	delete board;
 }
 
 /*
@@ -39,10 +46,45 @@ Player::~Player() {
  * The move returned must be legal; if there are no valid moves for your side,
  * return nullptr.
  */
-Move *Player::doMove(Move *opponentsMove, int msLeft) {
-    /*
-     * TODO: Implement how moves your AI should play here. You should first
-     * process the opponent's opponents move before calculating your own move
-     */
-    return nullptr;
+Move *Player::doMove(Move *opponentsMove, int msLeft)
+{
+	board->doMove(opponentsMove, (side == BLACK) ? WHITE : BLACK);
+	Move *bestMove = nullptr;
+    int maxScore = -9;
+    if (board->hasMoves(side))
+	{
+		for (int i = 0; i < 8; i ++)
+		{
+			for (int j = 0; j < 8; j ++)
+			{
+                Move *move = new Move(i, j);
+				if (board->checkMove(move, side))
+				{
+                    int resultScore = getResultScore(move);
+                    if (resultScore > maxScore)
+                    {
+                        bestMove = move;
+                        maxScore = resultScore;
+                    }
+                }	
+			}
+		}
+	}
+    board->doMove(bestMove, side);
+    score = maxScore;
+    return bestMove;
+}
+
+/*
+ * Given a possible move that the player could make, returns the score after the
+ * move has been made. 
+ */
+int Player::getResultScore(Move *move)
+{
+    Board *newBoard = board->copy();
+    newBoard->doMove(move, side);
+    int score;
+    score = valueMap[(move->getX() + 1) * (move->getY() + 1) - 1];
+    delete newBoard;
+    return score;
 }
--- player.hpp
+++ player.hpp
@@ -13,9 +13,35 @@ public:
     ~Player();
 
     Move *doMove(Move *opponentsMove, int msLeft);
+    int getResultScore(Move *move);
 
     // Flag to tell if the player is running within the test_minimax context
     bool testingMinimax;
+    // Side the player is on (WHITE or BLACK)
+    Side side;
+    // Board used to keep track of the moves made
+    Board *board;
+    // Number of tyles owned by player - number of tiles owned by opponent
+    int score;
+    /* Map of the values of each square
+	 *      _________________________________
+	 *		| 5 | -2| 3 | 3 | 3 | 3 | -2| 5 |
+	 *		|___|___|___|___|___|___|___|___|
+	 *      | -2| -2| -1| -1| -1| -1| -2| -2|
+	 *		|___|___|___|___|___|___|___|___|
+	 *      | 3 | -1| 2 | 1 | 1 | 2 | -1| 3 |
+	 *		|___|___|___|___|___|___|___|___|
+	 *      | 3 | -1| 1 | 1 | 1 | 1 | -1| 3 |
+	 *		|___|___|___|___|___|___|___|___|
+	 *      | 3 | -1| 1 | 1 | 1 | 1 | -1| 3 |
+	 *		|___|___|___|___|___|___|___|___|
+	 *      | 3 | -1| 2 | 1 | 1 | 2 | -1| 3 |
+	 *		|___|___|___|___|___|___|___|___|
+	 *      | -2| -2| -1| -1| -1| -1| -2| -2|
+	 *		|___|___|___|___|___|___|___|___|
+	 *		| 5 | -2| 3 | 3 | 3 | 3 | -2| 5 |
+	 *		|___|___|___|___|___|___|___|___|
+	 */
 };
 
 #endif
